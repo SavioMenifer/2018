40cba876865f1a79f516646abc45a5f04d8b55f6
saviomenifer <saviomenifer@gmail.com>
2018-10-02 12:49:37 +0530

    lotsa changes

---
+++ js/main.js
@@ -0,0 +1,112 @@
+jQuery(document).ready(function($){
+	var contentSections = $('.cd-section'),
+		navigationItems = $('#cd-vertical-nav a');
+
+	updateNavigation();
+	$(window).on('scroll', function(){
+		updateNavigation();
+	});
+
+	//smooth scroll to the section
+	navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+    //smooth scroll to second section
+    $('.cd-scroll-down').on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    //open-close navigation on touch devices
+    $('.touch .cd-nav-trigger').on('click', function(){
+    	$('.touch #cd-vertical-nav').toggleClass('open');
+  
+    });
+    //close navigation on touch devices when selectin an elemnt from the list
+    $('.touch #cd-vertical-nav a').on('click', function(){
+    	$('.touch #cd-vertical-nav').removeClass('open');
+    });
+
+	function updateNavigation() {
+		contentSections.each(function(){
+			$this = $(this);
+			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+				navigationItems.eq(activeSection).addClass('is-selected');
+			}else {
+				navigationItems.eq(activeSection).removeClass('is-selected');
+			}
+		});
+	}
+
+	function smoothScroll(target) {
+        $('body,html').animate(
+        	{'scrollTop':target.offset().top},
+        	600
+        );
+	}
+});
+
+$(document).ready(function() {
+	var page_url;
+  var ripple_wrap = $('.ripple-wrap'),
+      rippler = $('.ripple'),
+      finish = false,
+      monitor = function(el) {
+        var computed = window.getComputedStyle(el, null),
+            borderwidth = parseFloat(computed.getPropertyValue('border-left-width'));
+        if (!finish && borderwidth >= 1500) {
+          el.style.WebkitAnimationPlayState = "paused";
+          el.style.animationPlayState = "paused";
+          loadNewContent(page_url);
+        }
+        if (finish) {
+			if (page_url != window.location) {
+			//add the new page to the window.history
+			//if the new page was triggered by a 'popstate' event, don't add it
+			window.history.pushState({path: page_url}, '', page_url);
+			}
+          el.style.WebkitAnimationPlayState = "running";
+          el.style.animationPlayState = "running";
+          return;
+        } else {
+          window.requestAnimationFrame(function() {monitor(el)});
+        }
+      };
+  
+  rippler.bind("webkitAnimationEnd oAnimationEnd msAnimationEnd mozAnimationEnd animationend", function(e){
+    ripple_wrap.removeClass('goripple');
+  });
+
+  $('.section-link').on('click', function(e) {
+  	page_url = $(this).attr('href');
+    rippler.css('left', e.clientX + 'px');
+    rippler.css('top', e.clientY + 'px');
+    e.preventDefault();
+    finish = false;
+    ripple_wrap.addClass('goripple');
+    window.requestAnimationFrame(function() {monitor(rippler[0])});
+  });
+
+	function loadNewContent(url) {
+	    url = ('' == url) ? 'index.html' : url;
+	    var newSection = 'cd-' + url.replace('.html', '');
+	    var section = $('<div class="cd-main-content ' + newSection + '"></div>');
+
+	    section.load(url + ' .cd-main-content > *', function(event) {
+	        // load new content and replace <main> content with the new one
+	        $('main').html(section);
+
+	        setTimeout(function() {
+		 		finish = true;
+		    },10);
+	    });
+	}
+
+	$(window).bind("popstate", function() {
+	    link = location.pathname.replace(/^.*[\\/]/, ""); // get filename only
+	    loadNewContent(link);
+	});
+  
+});
